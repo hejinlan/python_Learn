3b5a253a3791d5883d8ef6a7b147d415795cc9d1
Hejinlan <70897155+hejinlan@users.noreply.github.com>
2021-08-15 15:06:43 +0800

    Update ActionChains.py

--- Selenium_py/ActionChains.py
+++ Selenium_py/ActionChains.py
@@ -14,5 +14,4 @@ driver.maximize_window()
 time.sleep(10)
 to_element =  driver.find_element_by_xpath('//*[contains(text(),"百度热搜")]' )
 ActionChains(driver).move_to_element_with_offset(to_element, 200, 300).context_click().perform()
-ActionChains(driver).move_by_offset(x=1150, y=769).click_and_hold().perform() # 鼠标左键点击， 200为x坐标， 100为y坐标
-ActionChains(driver).move_by_offset(x=1150, y=769).context_click().perform() # 鼠标右键点击
+
